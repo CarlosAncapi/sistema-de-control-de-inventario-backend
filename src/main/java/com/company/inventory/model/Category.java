38b7921f540bef0a150c826091839cd2430ecc0d
CarlosAncapi <115656721+CarlosAncapi@users.noreply.github.com>
2023-02-09 18:09:43 -0300

    añadiendo clase category

---
+++ src/main/java/com/company/inventory/model/Category.java
@@ -0,0 +1,23 @@
+package com.company.inventory.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="category")
+public class Category implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private String description;
+	
+	
+
+}
