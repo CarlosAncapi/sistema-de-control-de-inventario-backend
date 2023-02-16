b58b64c31f22e067c6779e53e17843d414f30450
CarlosAncapi <115656721+CarlosAncapi@users.noreply.github.com>
2023-02-16 17:12:12 -0300

    search category for Id

--- src/main/java/com/company/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.company.inventory.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,9 +20,12 @@ public class CategoryServiceImpl implements ICategoryService {
 	@Autowired
 	private ICategoryDao categoryDao;  
 
+	//-------------------------------------------------------------------------
+	
+	//buscar y devolver un listado con todas las categorias existentes en bd
+	
 	@Override
 	@Transactional(readOnly = true)
-	//buscar y devolver un listado con todas las categorias existentes en bd
 	public ResponseEntity<CategoryResponseRest> search() {
 		
 		CategoryResponseRest response = new CategoryResponseRest();
@@ -34,7 +39,42 @@ public class CategoryServiceImpl implements ICategoryService {
 			
 		}catch (Exception e) {
 			
-			response.setMetadata("Respuesta ok", "-1", "Error al consultar");
+			response.setMetadata("Respuesta nok", "-1", "Error al consultar");
+			e.getStackTrace();
+			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+	}
+	
+	//---------------------------------------------------------------------------------------------------------
+	
+	//buscar una categoria por id y devolverla 
+	
+	@Override
+	@Transactional(readOnly = true)
+	public ResponseEntity<CategoryResponseRest> searchById(Long id) {
+		
+		CategoryResponseRest response = new CategoryResponseRest();
+		List<Category> list = new ArrayList<>();
+		
+		
+		try {
+			Optional<Category> category = categoryDao.findById(id);
+			
+			if(category.isPresent()) {
+				list.add(category.get());
+				response.getCategoryResponse().setCategory(list);
+				response.setMetadata("Respuesta ok", "00", "Categoria encontrada");
+				
+			}else {
+				response.setMetadata("Respuesta nok", "-1", "Categoria no encontrada");
+				return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.NOT_FOUND);
+			}
+			
+		}catch (Exception e) {
+			
+			response.setMetadata("Respuesta nok", "-1", "Error al consultar por id");
 			e.getStackTrace();
 			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
